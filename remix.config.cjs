343f3b998d945147a60fa2f8df99bc28b0530664
Nawaf <nawafmahsoun11@gmail.com>
2025-04-17 19:23:43 +0300

    back to commit

---
+++ remix.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  serverBuildTarget: "vercel",
+  // Add any other Remix config options here as needed
+};
---
+++ remix.config.js
@@ -0,0 +1,5 @@
+// remix.config.js
+module.exports = {
+  serverBuildTarget: "vercel",
+  // Add any other Remix config options here as needed
+};
